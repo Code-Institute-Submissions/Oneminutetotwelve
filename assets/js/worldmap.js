399d4b1349520135b73b1774576666de00438b31
GurraT <gustafthomsson@hotmail.com>
2021-01-28 21:00:51 +0000

    Trying to find common key in coutries and pop array and add value into locations array when if statement is true

---
+++ assets/js/worldmap.js
@@ -0,0 +1,45 @@
+
+    let locations=[];
+
+     fetch("https://www.trackcorona.live/api/countries")
+          .then(function (res) {
+                return res.json();
+            })
+            .then(function (country) {
+                let i = 0;
+                let len = country.data.length;
+                for (i = 0; i < len; i++){
+                  locations.push({"country":country.data[i].location, "lat":country.data[i].latitude, "long":country.data[i].longitude});
+                } 
+    
+                //Init map
+        })
+            .catch(function (er) {
+                console.log('error: ' + er);
+            })
+
+ function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 3,
+    center: { lat: -28.024, lng: 140.887 },
+  })
+ var infowindow = new google.maps.InfoWindow;
+
+    var marker, a;
+
+    for (a = 0; a < locations.length; a++) {  
+        marker = new google.maps.Marker({
+             position: new google.maps.LatLng(locations[a].lat, locations[a].long),
+             map: map
+        })
+
+      google.maps.event.addListener(marker, 'click', (function(marker, a) {
+             return function() {
+                 infowindow.setContent(locations[a].country);
+                 infowindow.open(map, marker);
+             }
+        })(marker, a))
+    } 
+   
+}
+
