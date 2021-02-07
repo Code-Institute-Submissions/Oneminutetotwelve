a709f794bc681438a14ddd0a4de0237de913dda6
GurraT <gustafthomsson@hotmail.com>
2021-02-07 13:34:50 +0000

    Working with parallax function

---
+++ assets/js/addText.js
@@ -0,0 +1,4 @@
+document.getElementsByClassName("info1")[0].addEventListener('onmouseover', function(){
+    
+document.getElementsByClassName("info1")[0].innerHTML ="<p> The earth is ours to nurture and care for </p>"
+})
