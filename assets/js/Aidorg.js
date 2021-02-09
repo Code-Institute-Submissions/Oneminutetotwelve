af84c2bd9c187eabaf790445ad06b28dceb8b474
GurraT <gustafthomsson@hotmail.com>
2021-02-09 20:40:34 +0000

    Removed unused file, cleaned up code, next step structure js-code, update readme-file

--- assets/js/Aidorg.js
+++
@@ -1,9 +0,0 @@
-
-
-fetch("https://www.insamlingskontroll.se/autocomplete/json",
-{mode:'no-cors'})
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-          
-         
